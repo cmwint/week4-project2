489e9ddbcf0e33eb7cb96bd7ce2bfcf3b71d3717
Corinne Winthrop <corinne.winthrop@gmail.com>
2016-04-25 18:46:03 -0600

    Add practice solutions

--- practice.js
+++ practice.js
@@ -1,38 +1,74 @@
 $(document).ready(function(){
-  $("#1").click(function(){
-    $("#hidden1").css("display", "inline")
-  })
-  $("#2").click(function(){
-    $("#hidden2").css("display", "inline")
-  })
-  $("#3").click(function(){
-    $("#hidden3").css("display", "inline")
-  })
-  $("#4").click(function(){
-    $("#hidden4").css("display", "inline")
-  })
-  $("#5").click(function(){
-    $("#hidden5").css("display", "inline")
-  })
-  $("#6").click(function(){
-    $("#hidden6").css("display", "inline")
-  })
-  $("#7").click(function(){
-    $("#hidden7").css("display", "inline")
-  })
-  $("#8").click(function(){
-    $("#hidden8").css("display", "inline")
-  })
-  $("#9").click(function(){
-    $("#hidden9").css("display", "inline")
-  })
-  $("#10").click(function(){
-    $("#hidden10").css("display", "inline")
-  })
-  $("#11").click(function(){
-    $("#hidden11").css("display", "inline")
-  })
-  $("#12").click(function(){
-    $("#hidden12").css("display", "inline")
-  })
+	// Question #1
+	$('#submitBtn').click(function(){
+		var value = $('.textInput').val();
+		$(this).after('<h3>'+value+'</h3>');
+	});
+	// Question #2
+	$('.chex input').click(function(){
+		$('#message').toggleClass('hidden');
+	});
+	// Question #3
+	$('table tr:last').after('<tr><td>Lola</td><td>8</td><td>Sleepy</td></tr>');
+	// Question #4
+	$('td:contains("Hangry")').css('color','red');
+	$('td:contains("Content")').css('color','green');
+	// Question #5
+	$('.btn-warning').click(function(){
+		$('tbody tr').each(function(){
+			var children = $(this).find('td:nth-child(2)').text();
+			if(children < 10){
+				$(this).remove();
+			}
+		});
+	});
+	// Question #6
+	$('.btn-success').click(function(){
+		$('tbody tr').each(function(){
+			var children = $(this).find('td:nth-child(1)');
+			children.text(children.text().toUpperCase());
+		});
+	});
+	// Question #7
+	$('.btn-primary').click(function(){
+		$('.table-striped').toggle();
+	});
+	// Question #8
+	$('#disableUs input[type=text]').prop('disabled', true);
+	$('#disableUs input[type=checkbox]').prop('disabled', true);
+	$('#disableUs a').click(function(e){
+		e.preventDefault(); // prevents default behavior of an anchor tag
+	});
+	// Question #9
+	$(".dropdown").change(function(){
+		var value = $(this).find("option:selected").text();
+		$("<h3>" + value + "</h3>").insertAfter(".dropdown");
+	});
+	//Question #10:
+	var count = 0;
+	$("#alertList li").each(function(){
+		count += 1;
+	});
+	alert(count);
+	//Question #11:
+	$("<input type='button' class='btn btn-secondary' value='add'/>").insertBefore("#alertList");
+	$("<input type='button' class='btn btn-danger' value='remove'/>").insertBefore("#alertList");
+
+	$(".btn-secondary").click(function(){
+		$("#alertList").append("<li>New Item</li>");
+		counter += 1;
+		update();
+	});
+
+	$(".btn-danger").click(function(){
+		$("#alertList").children('li:last-child').remove();
+		counter -= 1;
+		update();
+	});
+
+	//Question #12:
+	$("p").last().hide();
+	var update = function(){
+		$("p:last").text("The count is: " + counter)
+	}
 });
